9c0fccac70129fde021ec5d1f0a2fb2e5dc7515d
ayushqui <113664933+ayushqui@users.noreply.github.com>
2024-09-15 07:33:27 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_camellia.mk
+    $(LOCAL_DIR)/arrow_camellia.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_camellia-user \
-    aosp_camellia-userdebug \
-    aosp_camellia-eng
+    arrow_camellia-user \
+    arrow_camellia-userdebug \
+    arrow_camellia-eng
